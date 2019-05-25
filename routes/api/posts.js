09b3d74d767d27e174e7c80fba117cb5da004e1f
Daniele Tortora <tortora87@gmail.com>
2019-05-25 16:32:09 +0100

    create routes to Add and Remove Likes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -92,8 +92,6 @@ router.delete('/:id', auth, async (req, res) => {
 
 		// Check on user
 		if (post.user.toString() !== req.user.id) {
-			console.log(post.user.toString());
-			console.log(req.user.id);
 			return res.status(401).json({ msg: 'User not authorized' });
 		}
 
@@ -105,6 +103,61 @@ router.delete('/:id', auth, async (req, res) => {
 		if (error.kind === 'ObjectId') {
 			return res.status(404).json({ msg: 'Post Not Found' });
 		}
+		res.status(500).send('Server Error');
+	}
+});
+
+// @route   PUT api/posts/like/:id
+// @desc    Like a post
+// @access  Private
+router.put('/like/:id', auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		// Check if post has been already liked
+		if (
+			post.likes.filter(like => like.user.toString() === req.user.id).length > 0
+		) {
+			return res.status(400).json({ msg: 'Post already liked' });
+		}
+
+		post.likes.unshift({ user: req.user.id });
+
+		await post.save();
+
+		return res.json(post.likes);
+	} catch (error) {
+		console.error(error.message);
+		res.status(500).send('Server Error');
+	}
+});
+
+// @route   PUT api/posts/unlike/:id
+// @desc    Unlike a post
+// @access  Private
+router.put('/unlike/:id', auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		// Check if post has been already liked
+		if (
+			post.likes.filter(like => like.user.toString() === req.user.id).length ===
+			0
+		) {
+			return res.status(400).json({ msg: 'Post has not yet been liked' });
+		}
+
+		// Get Remove Index
+		const removeIndex = post.likes
+			.map(like => like.user.toString())
+			.indexOf(req.user.id);
+		post.likes.splice(removeIndex, 1);
+
+		await post.save();
+
+		return res.json(post.likes);
+	} catch (error) {
+		console.error(error.message);
 		res.status(500).send('Server Error');
 	}
 });
