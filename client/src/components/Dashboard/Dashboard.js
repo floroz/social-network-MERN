73ac8a322d2f83b494ce2fc6f6b0e2b6c635d91c
Daniele Tortora <tortora87@gmail.com>
2019-05-26 15:12:32 +0100

    Implements features on Dashboard

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,12 +1,16 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { getCurrentProfile } from '../../actions/profile';
+import { getCurrentProfile, deleteAccount } from '../../actions/profile';
 import Spinner from '../Layout/Spinner';
+import DashboardActions from './DashboardActions';
 import { Link } from 'react-router-dom';
+import Experience from './Experience';
+import Education from './Education';
 
 const Dashboard = ({
 	getCurrentProfile,
+	deleteAccount,
 	auth: { user },
 	profile: { profile, loading }
 }) => {
@@ -23,7 +27,17 @@ const Dashboard = ({
 				<i className="fas fa-user" /> Welcome {user && user.name}
 			</p>{' '}
 			{profile !== null ? (
-				<>has</>
+				<>
+					<DashboardActions />
+					<Experience experience={profile.experience} />
+					<Education education={profile.education} />
+
+					<div className="my-2">
+						<button className="btn btn-danger" onClick={() => deleteAccount()}>
+							<i className="fas fas-user-minus" /> Delete My Account
+						</button>
+					</div>
+				</>
 			) : (
 				<>
 					<p>You have not yet setup a profile, please add some info</p>
@@ -39,7 +53,8 @@ const Dashboard = ({
 Dashboard.propTypes = {
 	getCurrentProfile: PropTypes.func.isRequired,
 	auth: PropTypes.object.isRequired,
-	profile: PropTypes.object.isRequired
+	profile: PropTypes.object.isRequired,
+	deleteAccount: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
@@ -49,5 +64,5 @@ const mapStateToProps = state => ({
 
 export default connect(
 	mapStateToProps,
-	{ getCurrentProfile }
+	{ getCurrentProfile, deleteAccount }
 )(Dashboard);
