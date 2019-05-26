c9de0316ed94ee1de70c255ce8ae9c0d7595a0aa
Daniele Tortora <tortora87@gmail.com>
2019-05-26 12:23:58 +0100

    added types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,10 +1,23 @@
 // Alert
 export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
+
 // Register User
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 export const REGISTER_FAIL = 'REGISTER_FAIL';
 
+// Login
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+
+// Logout
+export const LOGOUT = 'LOGOUT';
+
 // Auth
 export const USER_LOADED = 'USER_LOADED';
 export const AUTH_ERROR = 'AUTH_ERROR';
+
+// Profile
+export const PROFILE_ERROR = 'PROFILE_ERROR';
+export const GET_PROFILE = 'GET_PROFILE';
+export const CLEAR_PROFILE = 'CLEAR_PROFILE';
