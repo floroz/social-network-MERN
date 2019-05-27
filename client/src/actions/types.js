8aee0fab11db601bc125750d6d8d987e1a1fea16
Daniele Tortora <tortora87@gmail.com>
2019-05-27 15:01:42 +0100

    add remove delete post and likes

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -25,3 +25,9 @@ export const GET_REPOS = 'GET_REPOS';
 export const CLEAR_PROFILE = 'CLEAR_PROFILE';
 export const UPDATE_PROFILE = 'UPDATE_PROFILE';
 export const ACCOUNT_DELETED = 'ACCOUNT_DELETED';
+
+// Post
+export const GET_POSTS = 'GET_POSTS';
+export const POST_ERROR = 'POST_ERROR';
+export const UPDATE_LIKES = 'UPDATE_LIKES';
+export const DELETE_POST = 'DELETE_POST';
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,43 @@
+import React, { useEffect, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getPosts } from '../../actions//post';
+import Spinner from '../Layout/Spinner';
+import PostItem from './PostItem';
+
+const Posts = ({ getPosts, post: { posts, loading } }) => {
+	useEffect(() => {
+		getPosts();
+	}, [getPosts]);
+	return loading ? (
+		<Spinner />
+	) : (
+		<Fragment>
+			<h1 className="large text-primary">Posts</h1>
+			<p className="lead">
+				<i className="fas fa-user" /> Welcome to the Community
+			</p>
+			<div className="posts">
+				{posts && posts.length > 0 ? (
+					posts.map(post => <PostItem key={post._id} post={post} />)
+				) : (
+					<p>There are no posts...</p>
+				)}
+			</div>
+		</Fragment>
+	);
+};
+
+Posts.propTypes = {
+	post: PropTypes.object.isRequired,
+	getPosts: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+	post: state.post
+});
+
+export default connect(
+	mapStateToProps,
+	{ getPosts }
+)(Posts);
