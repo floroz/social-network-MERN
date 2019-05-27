bd8cbd7ca80d76c33c9d4414553bf274e1990c8a
Daniele Tortora <tortora87@gmail.com>
2019-05-27 12:01:34 +0100

    actions for all profiles in redux

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,9 +1,11 @@
 import {
 	PROFILE_ERROR,
 	GET_PROFILE,
+	GET_PROFILES,
 	UPDATE_PROFILE,
 	ACCOUNT_DELETED,
-	CLEAR_PROFILE
+	CLEAR_PROFILE,
+	GET_REPOS
 } from './types';
 import { setAlert } from './alert';
 import axios from 'axios';
@@ -14,6 +16,57 @@ export const getCurrentProfile = () => async dispatch => {
 		const res = await axios.get('/api/profile/me');
 		dispatch({
 			type: GET_PROFILE,
+			payload: res.data
+		});
+	} catch (error) {
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: error.response.statusText, status: error.response.status }
+		});
+	}
+};
+
+// Get all Profiles
+export const getProfiles = () => async dispatch => {
+	dispatch({
+		type: CLEAR_PROFILE
+	});
+	try {
+		const res = await axios.get('/api/profile/');
+		dispatch({
+			type: GET_PROFILES,
+			payload: res.data
+		});
+	} catch (error) {
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: error.response.statusText, status: error.response.status }
+		});
+	}
+};
+
+//Get Profile by ID
+export const getProfileById = userId => async dispatch => {
+	try {
+		const res = await axios.get(`/api/profile/user/${userId}`);
+		dispatch({
+			type: GET_PROFILE,
+			payload: res.data
+		});
+	} catch (error) {
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: error.response.statusText, status: error.response.status }
+		});
+	}
+};
+
+//Get GitHub Repos
+export const getGithubRepos = githubUserName => async dispatch => {
+	try {
+		const res = await axios.get(`/api/profile/github/${githubUserName}`);
+		dispatch({
+			type: GET_REPOS,
 			payload: res.data
 		});
 	} catch (error) {
